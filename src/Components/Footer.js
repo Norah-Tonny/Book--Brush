32182e7c73c8de0fdc102114f65263344e242401
phibymaty <phibymaty7@gmail.com>
2022-10-11 14:29:01 -0400

    added Footlist at the Footer file

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,12 +6,43 @@ const FooterList = styled.li``
 const Footer = () => {
     return (
         <FooterContainer>
-          
-            <FooterUl>
-                <FooterList>
-                    
-                </FooterList>
-            </FooterUl>  
+
+    <FooterContainer>
+      <FooterUl>
+        <FooterHeading>
+        Menu
+        </FooterHeading>
+        <FooterList>Home</FooterList>
+        <FooterList>Login</FooterList>
+        <FooterList>Results</FooterList>
+      </FooterUl>
+
+      <FooterUl>
+      <FooterHeading>
+       TOOLS
+       </FooterHeading>
+        <FooterList>Custom Creator</FooterList>
+        <FooterList>Instant Mockups</FooterList>
+        <FooterList>Trailer Creator</FooterList>
+        <FooterList>Cover Creator</FooterList>
+        <FooterList>Box Set Creator</FooterList>
+     
+      </FooterUl>
+      <Footer>
+
+      <FooterHeading>
+        Location
+        </FooterHeading>
+        <FooterList>Nairobi</FooterList>
+        <FooterList>Nakuru </FooterList>
+        <FooterList>Thika</FooterList>
+        <FooterList>Kirinyaga</FooterList>
+      </Footer>
+      <FooterUl>
+      <FooterList>Contact us through 0721346578/0798674532</FooterList>
+      </FooterUl>
+    </FooterContainer>
+  
         </FooterContainer>
     )
 }
