2d5bb46cd847d41c2e741d4565edfdaee26fffd7
jameskitonga <jameskitonga432@gmail.com>
2022-10-12 08:43:52 +0300

    modified app.js,categoriesItems.js,Result.js and Home.js

--- src/Components/CategoriesItems.js
+++ src/Components/CategoriesItems.js
@@ -1,12 +1,35 @@
 import styled from "styled-components"
+import { Categories } from "../data";
+
+
+
 
 const CategoryContainer = styled.div``
-
+const CategoriesTitle=styled.h4``
+const BookContainer = styled.div``
+const BookImg = styled.img``
+const Content=styled.div``
 
 const CategoriesItems = () => {
     return (
         <CategoryContainer>
+            {
+                Categories.map((Category, index) => {
+                    return (
 
+                        <BookContainer key={index}>
+                            <CategoriesTitle>{Category.name}</CategoriesTitle>
+                            
+                            <BookImg>
+                            <Content>{Category.img}</Content>
+                            </BookImg>
+
+                        </BookContainer>
+
+                    )
+
+                })
+            }
 
             
         </CategoryContainer>
--- src/Components/Results.js
+++ src/Components/Results.js
@@ -3,10 +3,17 @@ import styled from "styled-components";
 
 
 const ResultContainer = styled.div``
+const PrevLeft = styled.div``
+const PrevRight = styled.div``
+const SelectItems = styled.select``
+const OptionsItems=styled.option``
 
 const Results = () => {
     return (
         <ResultContainer>
+
+            
+
             
         </ResultContainer>
     )
